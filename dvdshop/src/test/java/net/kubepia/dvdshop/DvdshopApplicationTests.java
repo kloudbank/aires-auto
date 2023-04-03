eaff4adfccb263ed18edf46ef250d69f61a62291
nogada <seungil.han@gmail.com>
2023-04-03 14:07:27 +0900

    setup postgre dvdshop data

---
+++ dvdshop/src/test/java/net/kubepia/dvdshop/DvdshopApplicationTests.java
@@ -0,0 +1,13 @@
+package net.kubepia.dvdshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DvdshopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
